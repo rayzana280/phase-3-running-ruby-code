d740fbcfe4635864fe982d0051a35a8a4361f804
Ray Zana <rayzana280@gmail.com>
2023-08-06 12:14:11 -0400

    commit tree fdc07191e8851068787b33a4c34439c14fe2074a

--- app.rb
+++ app.rb
@@ -1,12 +1,20 @@
-# pp [{ id: 1, hello: "World" }, 
-#     { id: 2, hello: "Ruby" }, 
-#     { id: 3, hello: "Moon" }, 
-#     { id: 4, hello: "Learner" }]
+#pp [{ id: 1, hello: "World" }, 
+ #   { id: 2, hello: "Ruby" }, 
+ #  { id: 3, hello: "Moon" }, 
+  #  { id: 4, hello: "Learner" }]
+
+  puts "Hello World!"
+  print "Pass this test, please."
+  p [1,2,3]
+
+#p [1,2,3]
+#print "Hello World!"
+#print "Hello World!"
+
+# print "Pass this test, please."
 
 # p [1,2,3]
-# puts "Hello World!"
-# print "Pass this test, please."
-# p [1,2,3]
+
 
 # def add
     
@@ -14,10 +22,10 @@
 # # 1 + 1
 # puts add + add
 
-def add num1, num2
-    puts num1
-    puts num2
-    num1 + num2
-end
+#def add num1, num2
+   # puts num1
+  #  puts num2
+  #  num1 + num2
+#end
 
-puts add(1,3) + add(2,2)
+#puts add(1,3) + add(2,2)
